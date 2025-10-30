470b0c109c589ecff94e294cbdade34dd3bd67a1
iAtog <atogmc58vani@gmail.com>
2025-10-30 09:43:54 -0400

    feat: initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace fluxguard
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.projectUrlTxt = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backendToggle = new System.Windows.Forms.Button();
+            this.embeddingsToggle = new System.Windows.Forms.Button();
+            this.backendEstado = new System.Windows.Forms.Label();
+            this.embeddingsEstado = new System.Windows.Forms.Label();
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
+            this.backendLogBox = new System.Windows.Forms.RichTextBox();
+            this.embeddingsLogBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // projectUrlTxt
+            // 
+            this.projectUrlTxt.Location = new System.Drawing.Point(12, 12);
+            this.projectUrlTxt.Name = "projectUrlTxt";
+            this.projectUrlTxt.ReadOnly = true;
+            this.projectUrlTxt.Size = new System.Drawing.Size(568, 20);
+            this.projectUrlTxt.TabIndex = 0;
+            this.projectUrlTxt.Text = "C:\\Users\\soperaciones\\Desktop\\neuroflux";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Fluxguard (Pastoflux)";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(97, 26);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // backendToggle
+            // 
+            this.backendToggle.Location = new System.Drawing.Point(299, 46);
+            this.backendToggle.Name = "backendToggle";
+            this.backendToggle.Size = new System.Drawing.Size(75, 23);
+            this.backendToggle.TabIndex = 3;
+            this.backendToggle.Text = "Servidor";
+            this.backendToggle.UseVisualStyleBackColor = true;
+            this.backendToggle.Click += new System.EventHandler(this.backendToggle_Click);
+            // 
+            // embeddingsToggle
+            // 
+            this.embeddingsToggle.Location = new System.Drawing.Point(12, 46);
+            this.embeddingsToggle.Name = "embeddingsToggle";
+            this.embeddingsToggle.Size = new System.Drawing.Size(75, 23);
+            this.embeddingsToggle.TabIndex = 7;
+            this.embeddingsToggle.Text = "Embeddings";
+            this.embeddingsToggle.UseVisualStyleBackColor = true;
+            this.embeddingsToggle.Click += new System.EventHandler(this.embeddingsToggle_Click);
+            // 
+            // backendEstado
+            // 
+            this.backendEstado.AutoSize = true;
+            this.backendEstado.Location = new System.Drawing.Point(380, 51);
+            this.backendEstado.Name = "backendEstado";
+            this.backendEstado.Size = new System.Drawing.Size(31, 13);
+            this.backendEstado.TabIndex = 4;
+            this.backendEstado.Text = "--------";
+            // 
+            // embeddingsEstado
+            // 
+            this.embeddingsEstado.AutoSize = true;
+            this.embeddingsEstado.Location = new System.Drawing.Point(93, 51);
+            this.embeddingsEstado.Name = "embeddingsEstado";
+            this.embeddingsEstado.Size = new System.Drawing.Size(31, 13);
+            this.embeddingsEstado.TabIndex = 8;
+            this.embeddingsEstado.Text = "--------";
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Interval = 5000;
+            this.checkTimer.Tick += new System.EventHandler(this.checkTimer_Tick);
+            // 
+            // backendLogBox
+            // 
+            this.backendLogBox.BackColor = System.Drawing.Color.Black;
+            this.backendLogBox.ForeColor = System.Drawing.Color.LightGreen;
+            this.backendLogBox.Location = new System.Drawing.Point(299, 75);
+            this.backendLogBox.Name = "backendLogBox";
+            this.backendLogBox.ReadOnly = true;
+            this.backendLogBox.Size = new System.Drawing.Size(280, 194);
+            this.backendLogBox.TabIndex = 9;
+            this.backendLogBox.Text = "";
+            // 
+            // embeddingsLogBox
+            // 
+            this.embeddingsLogBox.BackColor = System.Drawing.Color.Black;
+            this.embeddingsLogBox.ForeColor = System.Drawing.Color.LightGreen;
+            this.embeddingsLogBox.Location = new System.Drawing.Point(12, 75);
+            this.embeddingsLogBox.Name = "embeddingsLogBox";
+            this.embeddingsLogBox.ReadOnly = true;
+            this.embeddingsLogBox.Size = new System.Drawing.Size(280, 194);
+            this.embeddingsLogBox.TabIndex = 10;
+            this.embeddingsLogBox.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 276);
+            this.Controls.Add(this.projectUrlTxt);
+            this.Controls.Add(this.backendToggle);
+            this.Controls.Add(this.backendEstado);
+            this.Controls.Add(this.embeddingsToggle);
+            this.Controls.Add(this.embeddingsEstado);
+            this.Controls.Add(this.backendLogBox);
+            this.Controls.Add(this.embeddingsLogBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fluxguard - servicio para iniciar Pastoflux";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox projectUrlTxt;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Button backendToggle;
+        private System.Windows.Forms.Button embeddingsToggle;
+        private System.Windows.Forms.Label backendEstado;
+        private System.Windows.Forms.Label embeddingsEstado;
+        private System.Windows.Forms.Timer checkTimer;
+        private System.Windows.Forms.RichTextBox backendLogBox;
+        private System.Windows.Forms.RichTextBox embeddingsLogBox;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fluxguard
+{
+    public partial class Main : Form
+    {
+        private Process backendProcess;
+        private Process embeddingsProcess;
+
+        private volatile bool backendStartedDetected = false;
+        private volatile bool embeddingsStartedDetected = false;
+
+        public Main()
+        {
+            InitializeComponent();
+            this.Resize += hideInTray;
+
+            notifyIcon1.Visible = false;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.BalloonTipTitle = "Fluxguard";
+            notifyIcon1.BalloonTipText = "La aplicación se ha minimizado en segundo plano.";
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+
+            checkTimer.Enabled = true;
+        }
+
+        private void hideInTray(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                this.Show();
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            UpdateEstadoLabels(false, false);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized || !this.Visible)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.BringToFront();
+                this.Activate();
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void checkTimer_Tick(object sender, EventArgs e)
+        {
+            bool backendOn = backendStartedDetected;
+            bool embeddingsOn = embeddingsStartedDetected;
+
+            if (backendProcess != null && backendProcess.HasExited)
+            {
+                backendProcess.Dispose();
+                backendProcess = null;
+                backendStartedDetected = false;
+                AppendBackendLog("[INFO] Se finalizó el proceso del servidor");
+            }
+            if (embeddingsProcess != null && embeddingsProcess.HasExited)
+            {
+                embeddingsProcess.Dispose();
+                embeddingsProcess = null;
+                embeddingsStartedDetected = false;
+                AppendEmbeddingsLog("[INFO] Se finalizó el proceso de embeddings");
+            }
+
+            UpdateEstadoLabels(backendOn, embeddingsOn);
+        }
+
+        private void AppendBackendLog(string text)
+        {
+            if (backendLogBox.InvokeRequired)
+            {
+                backendLogBox.BeginInvoke(new Action(() => AppendBackendLog(text)));
+                return;
+            }
+            backendLogBox.AppendText(text + Environment.NewLine);
+            backendLogBox.ScrollToCaret();
+        }
+
+        private void AppendEmbeddingsLog(string text)
+        {
+            if (embeddingsLogBox.InvokeRequired)
+            {
+                embeddingsLogBox.BeginInvoke(new Action(() => AppendEmbeddingsLog(text)));
+                return;
+            }
+            embeddingsLogBox.AppendText(text + Environment.NewLine);
+            embeddingsLogBox.ScrollToCaret();
+        }
+
+        private void UpdateEstadoLabels(bool backendOn, bool embeddingsOn)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new Action(() => UpdateEstadoLabels(backendOn, embeddingsOn)));
+                return;
+            }
+
+            backendEstado.Text = state(backendOn, backendProcess);
+            backendEstado.ForeColor = color(backendOn, backendProcess);
+
+            embeddingsEstado.Text = state(embeddingsOn, embeddingsProcess);
+            embeddingsEstado.ForeColor = color(embeddingsOn, embeddingsProcess);
+        }
+
+        private string state(bool srv, object process)
+        {
+            return srv ? "Encendido" : (process != null ? "Iniciando..." : "Apagado");
+        }
+
+        private Color color(bool srv, object process)
+        {
+            return srv ? Color.Green : (process != null ? Color.Orange : Color.Red);
+        }
+
+
+        private void backendToggle_Click(object sender, EventArgs e)
+        {
+            if (backendProcess != null && !backendProcess.HasExited)
+            {
+                try
+                {
+                    AppendBackendLog("[INFO] Deteniendo servidor...");
+                    KillProcessTreeByPid(backendProcess.Id);
+                    backendProcess.WaitForExit(2000);
+                }
+                catch (Exception ex)
+                {
+                    AppendBackendLog("[INFO] Error deteniendo servidor: " + ex.Message);
+                }
+                finally
+                {
+                    try { backendProcess.Kill(); } catch { }
+                    try { backendProcess.Dispose(); } catch { }
+                    backendProcess = null;
+                    backendStartedDetected = false;
+                    UpdateEstadoLabels(false, embeddingsStartedDetected);
+                }
+                return;
+            }
+
+            string folder = projectUrlTxt.Text.Trim();
+            if (string.IsNullOrEmpty(folder))
+            {
+                MessageBox.Show("No se puede dejar vacío la dirección del proyecto en disco", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            AppendBackendLog("[INFO] Iniciando servidor en: " + folder);
+            backendStartedDetected = false;
+            UpdateEstadoLabels(false, embeddingsStartedDetected);
+
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c npm start",
+                    WorkingDirectory = folder,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                backendProcess = new Process { StartInfo = psi, EnableRaisingEvents = true };
+                backendProcess.OutputDataReceived += (s, ev) =>
+                {
+                    if (ev.Data == null) return;
+                    AppendBackendLog(ev.Data);
+                    if (!backendStartedDetected && ev.Data.IndexOf("Server listening at", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        backendStartedDetected = true;
+                        UpdateEstadoLabels(true, embeddingsStartedDetected);
+                    }
+                };
+                backendProcess.ErrorDataReceived += (s, ev) =>
+                {
+                    if (ev.Data == null) return;
+                    AppendBackendLog("[err] " + ev.Data);
+                    if (!backendStartedDetected && ev.Data.IndexOf("Server listening at", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        backendStartedDetected = true;
+                        UpdateEstadoLabels(true, embeddingsStartedDetected);
+                    }
+                };
+                backendProcess.Exited += (s, ev) =>
+                {
+                    backendStartedDetected = false;
+                    AppendBackendLog("[INFO] Servidor finalizado");
+                };
+
+                backendProcess.Start();
+                backendProcess.BeginOutputReadLine();
+                backendProcess.BeginErrorReadLine();
+
+                UpdateEstadoLabels(false, embeddingsStartedDetected);
+            }
+            catch (Exception ex)
+            {
+                AppendBackendLog("[ERROR] No se pudo iniciar backend: " + ex.Message);
+                UpdateEstadoLabels(false, embeddingsStartedDetected);
+            }
+        }
+
+        private void embeddingsToggle_Click(object sender, EventArgs e)
+        {
+            if (embeddingsProcess != null && !embeddingsProcess.HasExited)
+            {
+                try
+                {
+                    AppendEmbeddingsLog("[INFO] Deteniendo embeddings...");
+                    KillProcessTreeByPid(embeddingsProcess.Id);
+                    embeddingsProcess.WaitForExit(2000);
+                }
+                catch (Exception ex)
+                {
+                    AppendEmbeddingsLog("[ERROR] Error deteniendo embeddings: " + ex.Message);
+                }
+                finally
+                {
+                    try { embeddingsProcess.Kill(); } catch { }
+                    try { embeddingsProcess.Dispose(); } catch { }
+                    embeddingsProcess = null;
+                    embeddingsStartedDetected = false;
+                    UpdateEstadoLabels(backendStartedDetected, false);
+                }
+                return;
+            }
+
+            string folder = projectUrlTxt.Text.Trim();
+            if (string.IsNullOrEmpty(folder))
+            {
+                MessageBox.Show("No se puede dejar vacío la dirección del proyecto en disco", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            AppendEmbeddingsLog("[INFO] Iniciando embeddings en: " + folder);
+            embeddingsStartedDetected = false;
+            UpdateEstadoLabels(backendStartedDetected, false);
+
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c npm run embeddings",
+                    WorkingDirectory = folder,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                };
+
+                embeddingsProcess = new Process { StartInfo = psi, EnableRaisingEvents = true };
+                embeddingsProcess.OutputDataReceived += (s, ev) =>
+                {
+                    if (ev.Data == null) return;
+                    AppendEmbeddingsLog(ev.Data);
+                    if (!embeddingsStartedDetected && ev.Data.IndexOf("ready, listening", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        embeddingsStartedDetected = true;
+                        UpdateEstadoLabels(backendStartedDetected, true);
+                    }
+                };
+                embeddingsProcess.ErrorDataReceived += (s, ev) =>
+                {
+                    if (ev.Data == null) return;
+                    AppendEmbeddingsLog("[ERR] " + ev.Data);
+                    if (!embeddingsStartedDetected && ev.Data.IndexOf("ready, listening", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        embeddingsStartedDetected = true;
+                        UpdateEstadoLabels(backendStartedDetected, true);
+                    }
+                };
+                embeddingsProcess.Exited += (s, ev) =>
+                {
+                    embeddingsStartedDetected = false;
+                    AppendEmbeddingsLog("[INFO] Embeddings finalizado");
+                };
+
+                embeddingsProcess.Start();
+                embeddingsProcess.BeginOutputReadLine();
+                embeddingsProcess.BeginErrorReadLine();
+
+                UpdateEstadoLabels(backendStartedDetected, false);
+            }
+            catch (Exception ex)
+            {
+                AppendEmbeddingsLog("[Error] No se pudo iniciar embeddings: " + ex.Message);
+                UpdateEstadoLabels(backendStartedDetected, false);
+            }
+        }
+
+        private void KillProcessTreeByPid(int pid)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "taskkill",
+                    Arguments = $"/PID {pid} /T /F",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+                using (var p = Process.Start(psi))
+                {
+                    p.WaitForExit(3000);
+                }
+            }
+            catch { }
+        }
+
+        private void StopProcess(ref Process proc, ref bool startedDetected, Action<string> appendLog, string name)
+        {
+            if (proc == null) return;
+            try
+            {
+                appendLog($"[INFO] deteniendo {name} (pid={proc.Id})...");
+                KillProcessTreeByPid(proc.Id);
+                if (!proc.WaitForExit(5000))
+                {
+                    try { proc.Kill(); } catch { }
+                }
+            }
+            catch (Exception ex)
+            {
+                try { appendLog("[ERROR] Error deteniendo " + name + ": " + ex.Message); } catch { }
+            }
+            finally
+            {
+                try { proc.Dispose(); } catch { }
+                proc = null;
+                startedDetected = false;
+                try { appendLog("[INFO] " + name + " detenido"); } catch { }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            checkTimer.Enabled = false;
+            StopProcess(ref backendProcess, ref backendStartedDetected, AppendBackendLog, "backend");
+            StopProcess(ref embeddingsProcess, ref embeddingsStartedDetected, AppendEmbeddingsLog, "embeddings");
+            base.OnFormClosing(e);
+        }
+    }
+}
